f0192d5784a331c2b3d947fa3f0a8a94c3b17697
december183 <guanyue584@163.com>
2018-02-22 16:25:24 +0800

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,10 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
     public function show(User $user) {
         return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user) {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, User $user) {
+        $request->validate([
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'.Auth::id(),
+            'email' => 'required|email',
+            'introduction' => 'max:80',
+        ]);
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
