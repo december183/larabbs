58655df3549c210551a74e6bb5efc72c30f1a6f0
december183 <guanyue584@163.com>
2018-03-07 14:36:10 +0800

    Keep track of user last visited time

---
+++ app/Models/Traits/LastActivedAtHelper.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models\Traits;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Redis;
+
+trait LastActivedAtHelper {
+    // 缓存相关
+    protected $hash_prefix = 'larabbs_last_actived_at_';
+    protected $field_prefix = 'user_';
+
+    public function recordLastActivedAt() {
+        // 获取今天的日期,如：2017-10-21
+        $date = Carbon::now()->toDateString();
+
+        // Redis 哈希表的命名,如：larabbs_last_actived_at_2017-10-21
+        $hash = $this->getHashFromDateString($date);
+
+        // 字段名称
+        $field = $this->getHashField();
+
+        // 当前时间,如：2017-10-21 08:35:15
+        $now = Carbon::now()->toDateTimeString();
+
+        // 数据写入Redis,如果字段已存在会被更新
+        Redis::hSet($hash, $field, $now);
+    }
+
+    public function syncUserActivedAt() {
+        // 获取昨天的日期，格式如：2017-10-21
+        $yestoday_date = Carbon::now()-subDay()->toDateString();
+
+        // Redis 哈希表的命名,如：larabbs_last_actived_at_2017-10-21
+        $hash = $this->getHashFromDateString($yestoday_date);
+
+        // 从Redis 中获取所有哈希表里的数据
+        $dates = Redis::hGetAll($hash);
+
+        // 遍历，并同步到数据库
+        foreach($dates as $user_id => $actived_at) {
+            // 将`user_1`转换为1
+            $user_id = str_replace($this->field_prefix, '', $user_id);
+
+            // 只有当用户存在时才更新到数据库中
+            if ($user = $this->find($user_id)) {
+                $user->last_actived_at = $actived_at;
+                $user->save();
+            }
+        }
+
+        // 以数据库为中心的存储，既已同步，即可删除
+        Redis::del($hash);
+    }
+
+    public function getLastActivedAtAttribute($value) {
+        // 获取今天的日期
+        $date = Carbon::now()->toDateString();
+
+        // Redis 哈希表的命名，如：larabbs_last_actived_at_2017-10-21
+        $hash = $this->getHashFromDateString($date);
+
+        // 字段名称, 如：`user_1`
+        $field = $this->getHashField();
+
+        // 优先使用Redis 的数据，否则使用数据库中
+        $datetime = Redis::hGet($hash, $field) ? : $value;
+
+        // 如果存在的话，返回时间对应的Carbon实例
+        if ($datetime) {
+            return new Carbon($datetime);
+        } else {
+            // 否则使用用户注册时间
+            return $this->created_at;
+        }
+    }
+
+    public function getHashFromDateString($date) {
+        // Redis 哈希表的命名， 如：larabbs_last_actived_at_2017-10-21
+        return $this->hash_prefix . $date;
+    }
+
+    public function getHashField() {
+        // 字段名称，如：user_1
+        return $this->field_prefix . $this->id;
+    }
+}
